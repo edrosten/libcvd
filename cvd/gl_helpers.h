63234b5dc22f42874aa556090c956fa4f976e446
pas1001 <pas1001>
2005-04-20 16:01:13 +0000

    Split some definitions into internal directory for readability

--- cvd/gl_helpers.h
+++ cvd/gl_helpers.h
@@ -1,7 +1,6 @@
 #ifndef CVD_GL_HELPERS_H
 #define CVD_GL_HELPERS_H
 
-
 #include <cvd/image_ref.h>
 #include <cvd/image.h>
 #include <cvd/byte.h>
@@ -12,155 +11,10 @@
 #include <numerics.h>
 #include <GL/gl.h>
 
+#include <cvd/internal/gl_types.h>
+
 namespace CVD
 {
-	
-	namespace gl
-	{
-		template<class C> struct data;
-
-		//Scalar types
-
-		template<> struct data<unsigned char>
-		{
-			static const int format=GL_LUMINANCE;
-			static const int type  =GL_UNSIGNED_BYTE;
-		};
-
-		template<> struct data<signed char>
-		{
-			static const int format=GL_LUMINANCE;
-			static const int type  =GL_BYTE;
-		};
-		
-		template<> struct data<unsigned  short>
-		{
-			static const int format=GL_LUMINANCE;
-			static const int type  =GL_UNSIGNED_SHORT;
-		};
-
-		template<> struct data<short>
-		{
-			static const int format=GL_LUMINANCE;
-			static const int type  =GL_SHORT;
-		};
-
-		template<> struct data<unsigned int>
-		{
-			static const int format=GL_LUMINANCE;
-			static const int type  =GL_UNSIGNED_INT;
-		};
-
-		template<> struct data<int>
-		{
-			static const int format=GL_LUMINANCE;
-			static const int type  =GL_INT;
-		};
-		
-		template<> struct data<float>
-		{
-			static const int format=GL_LUMINANCE;
-			static const int type  =GL_FLOAT;
-		};
-	
-
-		//Rgb<*> types
-	
-		template<> struct data<Rgb<unsigned char> >
-		{
-			static const int format=GL_RGB;
-			static const int type  =GL_UNSIGNED_BYTE;
-		};
-
-		template<> struct data<Rgb<signed char> >
-		{
-			static const int format=GL_RGB;
-			static const int type  =GL_BYTE;
-		};
-		
-		template<> struct data<Rgb<unsigned  short> >
-		{
-			static const int format=GL_RGB;
-			static const int type  =GL_UNSIGNED_SHORT;
-		};
-
-		template<> struct data<Rgb<short> >
-		{
-			static const int format=GL_RGB;
-			static const int type  =GL_SHORT;
-		};
-
-		template<> struct data<Rgb<unsigned int> >
-		{
-			static const int format=GL_RGB;
-			static const int type  =GL_UNSIGNED_INT;
-		};
-
-		template<> struct data<Rgb<int> >
-		{
-			static const int format=GL_RGB;
-			static const int type  =GL_INT;
-		};
-		
-		template<> struct data<Rgb<float> >
-		{
-			static const int format=GL_RGB;
-			static const int type  =GL_FLOAT;
-		};
-		
-		//Rgba<*> types
-	
-		template<> struct data<Rgba<unsigned char> >
-		{
-			static const int format=GL_RGBA;
-			static const int type  =GL_UNSIGNED_BYTE;
-		};
-
-		template<> struct data<Rgba<signed char> >
-		{
-			static const int format=GL_RGBA;
-			static const int type  =GL_BYTE;
-		};
-		
-		template<> struct data<Rgba<unsigned  short> >
-		{
-			static const int format=GL_RGBA;
-			static const int type  =GL_UNSIGNED_SHORT;
-		};
-
-		template<> struct data<Rgba<short> >
-		{
-			static const int format=GL_RGBA;
-			static const int type  =GL_SHORT;
-		};
-
-		template<> struct data<Rgba<unsigned int> >
-		{
-			static const int format=GL_RGBA;
-			static const int type  =GL_UNSIGNED_INT;
-		};
-
-		template<> struct data<Rgba<int> >
-		{
-			static const int format=GL_RGBA;
-			static const int type  =GL_INT;
-		};
-		
-		template<> struct data<Rgba<float> >
-		{
-			static const int format=GL_RGBA;
-			static const int type  =GL_FLOAT;
-		};
-		//Rgb8 type
-
-		template<> struct data<Rgb8>
-		{
-			static const int format=GL_RGBA;
-			static const int type  =GL_UNSIGNED_BYTE;
-		};
-		
-	};
-
 	
 	/// Specify the (x,y) co-ordinates of a vertex
 	/// @param i The vertex location
