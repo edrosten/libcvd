48357c4c0780c09afadfef611316f9fd904f90fe
aardvaark <aardvaark>
2005-07-07 13:50:36 +0000

    Changed to new TooN (namsepaced) version

--- cvd/vector_image_ref.h
+++ cvd/vector_image_ref.h
@@ -21,7 +21,7 @@
 #ifndef CVD_VECTOR_IMAGE_REF_H
 #define CVD_VECTOR_IMAGE_REF_H
 
-#include <numerics.h>
+#include <TooN/toon.h>
 #include <cvd/image_ref.h>
 
 namespace CVD
@@ -29,9 +29,9 @@ namespace CVD
 	/// Convert an image co-ordinate into a Vector
 	/// @param ir The ImageRef to convert
 	/// @ingroup gImage
-	inline Vector<2> vec(const ImageRef& ir)
+	inline TooN::Vector<2> vec(const ImageRef& ir)
 	{
-		Vector<2> r;
+		TooN::Vector<2> r;
 		r[0] = ir.x;
 		r[1] = ir.y;
 		return r;
@@ -40,7 +40,7 @@ namespace CVD
 	/// Convert a Vector into an image co-ordinate. Numbers are truncated, not rounded
 	/// @param v The Vector to convert
 	/// @ingroup gImage
-	inline ImageRef ir(const Vector<2>& v)
+	inline ImageRef ir(const TooN::Vector<2>& v)
 	{
 		return ImageRef((int)v[0], (int)v[1]);
 	}
