9355fd2351f7df30d4e57de457b065cc0bbe012d
edrosten <edrosten>
2005-08-22 12:36:44 +0000

    Added comments about saving to PS.

--- pnm_src/save_postscript.cxx
+++ pnm_src/save_postscript.cxx
@@ -31,6 +31,8 @@ namespace CVD
 
 void output_eps_header(ostream& o, int xs, int ys)
 {
+	//Standard EPS header. This is test more by concensus from various postscript handlers
+	//than with any specific reference to documentation, but it seems to work.
 	o << 
 	"%!PS-Adobe-3.0 EPSF-3.0"																"\n"
 	"%%BoundingBox: 0 0 " << xs << " " << ys << 											"\n"
@@ -38,6 +40,12 @@ void output_eps_header(ostream& o, int xs, int ys)
 	"%%Pages: 0"																			"\n"	
 	"%%EndComments"																			"\n"	
 	"%%EndProlog"																			"\n"
+	//This saves the dictionary state and enables a temporary dictionary.
+	//this should prevent the eps file from polluting the vm or dictionary.
+	//it's kind of the job of whatever includes EPS to do this, but this makes
+	//the job easier and improves reliability. Note that in Level 2 ps, the 
+	//dictionary can expand if more than 20 commands are added later.
+	//If custom PS is added, it must leave the stack empty at the end.
 	"save"																					"\n"
 	"20 dict begin"																			"\n";
 }
@@ -49,7 +57,10 @@ void output_eps_header(ostream& o, const ImageRef& s)
 
 void output_eps_footer(ostream& o)
 {
+	//End the temporary dictionary and restore the VM and graphics state.
 	o << "end restore\n"
+	//I'm not sure exactly what the end of an EPS image should be, but this 
+	//seems to work.
 	  << "%%EOF\n";
 }
 
@@ -60,7 +71,7 @@ namespace PNM
 string ps_out::bytes_to_base85(int n)
 {
 	//This function converts 4 raw bytes to 5 base-85 bytes. If there
-	//are less than 4 bytes, then the datat is zero padded.
+	//are less than 4 bytes, then the data is zero padded.
 
 	//Conversion to base85 is done the obvious way, but 33 is added
 	//to each number to make it a printable byte.
@@ -95,6 +106,13 @@ void ps_out::output_header()
 	//going from 0,0 - xs, ys. Since numbers are 72 per inch, the image is 72 dpi
 	o << " 0 " << ys << " translate 1 -1 scale " << xs << " " << ys << " 8 matrix  currentfile\n"
 		" /ASCII85Decode filter false " << m_channels << " colorimage\n";
+
+
+	// <xsize> <ysize> <bits per channel> <transform> <datasrc n> ... <datasrc 1> <multi> <num of channels> colorimage
+	// transform is an affine transformation matrix. We use the identity here, 
+	// and preform the the transform before the image, so that any subsequent
+	// graphics appear in the coordinate frame of the image.
+
 }
 
 
@@ -129,6 +147,7 @@ ps_out::~ps_out()
 
 void ps_out::write_raw_pixel_lines(const unsigned short* data, unsigned long nlines)
 {
+	//I think PS supports 12 bit images. They could be supported here.
 	throw CVD::Exceptions::Image_IO::WriteError("Postscript: Internal error: can not write 16bit Postscript image.");
 }
 
