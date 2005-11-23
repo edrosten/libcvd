7b9fb3c81deee96a8d9c65bccffa1bc721ad736b
edrosten <edrosten>
2005-11-23 17:23:53 +0000

    Added tree based 9,10,11 and 12 point FAST feature detection.

--- cvd/fast_corner.h
+++ cvd/fast_corner.h
@@ -26,8 +26,38 @@ namespace CVD
 	void fast_corner_detect(const BasicImage<byte>& im, std::vector<ImageRef>& corners, int barrier);
 	void fast_nonmax(const BasicImage<byte>& im, const std::vector<ImageRef>& corners, int barrier, std::vector<ImageRef>& nonmax_corners);
 	int corner_score(const BasicImage<byte>& im, ImageRef c, const int *pointer_dir, int barrier);
+	
+	/// Perform tree based 9 point FAST feature detection
+	///
+	/// @param im 		The input image
+	/// @param corners	The resulting container of corner locations
+	/// @param barrier	Corner detection threshold
+	/// @ingroup	corners
+	void fast_corner_detect_9(const BasicImage<byte>& im, std::vector<ImageRef>& corners, int barrier);
 
+	/// Perform tree based 10 point FAST feature detection
+	///
+	/// @param im 		The input image
+	/// @param corners	The resulting container of corner locations
+	/// @param barrier	Corner detection threshold
+	/// @ingroup	corners
+	void fast_corner_detect_10(const BasicImage<byte>& im, std::vector<ImageRef>& corners, int barrier);
 
+	/// Perform tree based 11 point FAST feature detection
+	///
+	/// @param im 		The input image
+	/// @param corners	The resulting container of corner locations
+	/// @param barrier	Corner detection threshold
+	/// @ingroup	corners
+	void fast_corner_detect_11(const BasicImage<byte>& im, std::vector<ImageRef>& corners, int barrier);
+
+	/// Perform tree based 12 point FAST feature detection
+	///
+	/// @param im 		The input image
+	/// @param corners	The resulting container of corner locations
+	/// @param barrier	Corner detection threshold
+	/// @ingroup	corners
+	void fast_corner_detect_12(const BasicImage<byte>& im, std::vector<ImageRef>& corners, int barrier);
 
 
 }
