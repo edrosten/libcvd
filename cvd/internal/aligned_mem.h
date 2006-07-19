5fb3078e4311d71e9c791e358185abaa034ada07
edrosten <edrosten>
2006-07-19 00:12:10 +0000

    Make aligned mem thread safe (provided the code is compiled with -pthread)

--- cvd/internal/aligned_mem.h
+++ cvd/internal/aligned_mem.h
@@ -2,11 +2,22 @@
 #define CVD_ALIGNED_MEM_H
 #include <map>
 #include <cassert>
+#include <cvd/config.h>
+
+#ifdef _REENTRANT
+    #ifndef CVD_HAVE_PTHREAD
+    	#error "CVD is not compiled with thread support. This code is not thread safe."
+    #else 
+	#include <pthread.h>
+    #endif
+#endif
 
 namespace CVD {
   namespace Internal
   {
-    
+
+    extern pthread_mutex_t aligned_mem_mutex;
+
     template <class T, int N=20> struct placement_delete
     {
       enum { Size = (1<<N) };
@@ -36,30 +47,58 @@ namespace CVD {
       static inline void free(T*, size_t ) {}
     };
 
+
     template <class T, int N> struct aligned_mem {
-      struct entry {
-	char* buffer;
-	size_t count;
-      };
-      static std::map<T*, entry> buffers;
-      static T* alloc(size_t count)
-      {
-	char* start = new char[count*sizeof(T) + N];
-	size_t val = (size_t)start;
-	T* astart = new (start + (N-(val % N))) T[count];
-	entry e = {start, count};
-	buffers[astart] = e;
-	return astart;
-      }
-      static void release(T* ptr) 
-      {
-	typename std::map<T*,entry>::iterator it = buffers.find(ptr);
-	assert(it != buffers.end());
-	placement_delete<T>::free(ptr, it->second.count);
-	delete[] it->second.buffer;
-	buffers.erase(it);
-      }
+	struct entry {
+	    char* buffer;
+	    size_t count;
+	};
+	static std::map<T*, entry> buffers;
+
+	static pthread_mutex_t mutex;
+
+
+	static T* alloc(size_t count)
+	{
+	    char* start = new char[count*sizeof(T) + N];
+	    size_t val = (size_t)start;
+	    T* astart = new (start + (N-(val % N))) T[count];
+	    entry e = {start, count};
+
+
+	    #if defined(CVD_HAVE_PTHREAD) && defined(_REENTRANT)
+	    pthread_mutex_lock(&mutex);
+	    #endif
+
+	    buffers[astart] = e;
+
+	    #if defined(CVD_HAVE_PTHREAD) && defined(_REENTRANT)
+	    pthread_mutex_unlock(&mutex);
+	    #endif
+
+	    return astart;
+	}
+	static void release(T* ptr) 
+	{
+	    #if defined(CVD_HAVE_PTHREAD) && defined(_REENTRANT)
+	    pthread_mutex_lock(&mutex);
+	    #endif
+
+	    typename std::map<T*,entry>::iterator it = buffers.find(ptr);
+	    assert(it != buffers.end());
+	    placement_delete<T>::free(ptr, it->second.count);
+	    delete[] it->second.buffer;
+
+
+	    buffers.erase(it);
+
+	    #if defined(CVD_HAVE_PTHREAD) && defined(_REENTRANT)
+	    pthread_mutex_unlock(&mutex);
+	    #endif
+	}
     };
+
+    template<class T, int N> pthread_mutex_t aligned_mem<T,N>::mutex = PTHREAD_MUTEX_INITIALIZER;
 
     template <class T, int N> std::map<T*,typename aligned_mem<T,N>::entry> aligned_mem<T,N>::buffers;
 
