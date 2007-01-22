015b9cac44d0ddd633b27a6bdfd8524ea948dfc7
ethaneade <ethaneade>
2007-01-22 14:23:37 +0000

    Added checks for alignment and minimum image size. Also changed whitespace, which cvs doesn't ignore.

--- cvd_src/faster_corner.cxx
+++ cvd_src/faster_corner.cxx
@@ -2,7 +2,7 @@
 
 #include <vector>
 #include <list>
-
+#include <cvd/utility.h>
 using namespace CVD;
 using namespace std;
 
@@ -15,100 +15,100 @@ namespace CVD
 
 
 
-struct Less { template <class T1, class T2> static bool eval(const T1 a, const T2 b) { return a < b; }};
-struct Greater { template <class T1, class T2> static bool eval(const T1 a, const T2 b) { return b < a; }};
+    struct Less { template <class T1, class T2> static bool eval(const T1 a, const T2 b) { return a < b; }};
+    struct Greater { template <class T1, class T2> static bool eval(const T1 a, const T2 b) { return b < a; }};
 
-template <class C> inline bool is_corner_12(const byte* p, int w, int t) {
-    const int w3 = 3*w;
-    if (!C::eval(p[-3],t)) {
-        if (!C::eval(p[-w3],t) || !C::eval(p[1-w3],t) || !C::eval(p[2-2*w],t) || !C::eval(p[3-w],t) || !C::eval(p[3],t) || !C::eval(p[3+w],t) || !C::eval(p[2+2*w],t) || !C::eval(p[1+w3],t) || !C::eval(p[w3],t))
-            return false;
-        if (!C::eval(p[-1-w3],t))
-            return (C::eval(p[-1+w3],t) && C::eval(p[-2+2*w],t) && C::eval(p[-3+w],t));
-        if (!C::eval(p[-2-2*w],t))
-            return (C::eval(p[-1+w3],t) && C::eval(p[-2+2*w],t));
-        if (!C::eval(p[-3-w],t))
-            return (C::eval(p[-1+w3],t));
-        return true;
-    }
-    if (!C::eval(p[-3-w],t)) {
-        if (!C::eval(p[1-w3],t) || !C::eval(p[2-2*w],t) || !C::eval(p[3-w],t) || !C::eval(p[3],t) || !C::eval(p[3+w],t) || !C::eval(p[2+2*w],t) || !C::eval(p[1+w3],t) || !C::eval(p[w3],t) || !C::eval(p[-1+w3],t))
-            return false;
-        if (!C::eval(p[-w3],t)  || !C::eval(p[-1-w3],t))
-            return (C::eval(p[-2+2*w],t) && C::eval(p[-3+w],t));
-        if (!C::eval(p[-2-2*w],t))
-            return (C::eval(p[-2+2*w],t));
-        return true;
-    }
-    if (!C::eval(p[-2-2*w],t)) {
-        if (!C::eval(p[2-2*w],t) || !C::eval(p[3-w],t) || !C::eval(p[3],t) || !C::eval(p[3+w],t) || !C::eval(p[2+2*w],t) || !C::eval(p[1+w3],t) || !C::eval(p[w3],t) || !C::eval(p[-1+w3],t) || !C::eval(p[-2+2*w],t))
-            return false;
-        if (C::eval(p[-3+w],t)) return true;
-        return (C::eval(p[1-w3],t)  && C::eval(p[-w3],t) && C::eval(p[-1-w3],t));
-    }
-    if (!C::eval(p[-1-w3],t)) {
-        return (C::eval(p[3-w],t) && C::eval(p[3],t) && C::eval(p[3+w],t) && C::eval(p[2+2*w],t) && C::eval(p[1+w3],t) && C::eval(p[w3],t) && C::eval(p[-1+w3],t) && C::eval(p[-2+2*w],t));
-    }
-    if (!C::eval(p[-w3],t)) {
-        return (C::eval(p[3],t) && C::eval(p[3+w],t) && C::eval(p[2+2*w],t) && C::eval(p[1+w3],t) && C::eval(p[w3],t) && C::eval(p[-1+w3],t) && C::eval(p[-2+2*w],t) && C::eval(p[-3+w],t));
-    }
-    if (!C::eval(p[1-w3],t)) {
-        return (C::eval(p[3+w],t) && C::eval(p[2+2*w],t) && C::eval(p[1+w3],t) && C::eval(p[w3],t) && C::eval(p[-1+w3],t) && C::eval(p[-2+2*w],t) && C::eval(p[-3+w],t));
-    }
-    if (!C::eval(p[2-2*w],t)) {
-        return (C::eval(p[2+2*w],t) && C::eval(p[1+w3],t) && C::eval(p[w3],t) && C::eval(p[-1+w3],t) && C::eval(p[-2+2*w],t) && C::eval(p[-3+w],t));
-    }
-    if (!C::eval(p[3-w],t)) {
-        return (C::eval(p[1+w3],t) && C::eval(p[w3],t) && C::eval(p[-1+w3],t) && C::eval(p[-2+2*w],t) && C::eval(p[-3+w],t));
-    }
-    if (!C::eval(p[3],t)) {
-        return (C::eval(p[w3],t) && C::eval(p[-1+w3],t) && C::eval(p[-2+2*w],t) && C::eval(p[-3+w],t));
-    }
-    if (!C::eval(p[3+w],t)) {
-        return (C::eval(p[-1+w3],t) && C::eval(p[-2+2*w],t) && C::eval(p[-3+w],t));
-    }
-    if (!C::eval(p[2+2*w],t)) {
-        return (C::eval(p[-2+2*w],t) && C::eval(p[-3+w],t));
-    }
-    if (!C::eval(p[1+w3],t)) {
-        return (C::eval(p[-3+w],t));
-    }
-    return true;
-}
-
-
-template <int I, int N> struct BitCheck {
-    template <class C> static inline void eval(unsigned int three, const byte* p, const int w, const int barrier, C& corners) {
-	const int BIT = 1<<I;
-	if (three & BIT) {
-	    if (three & (BIT << 16)) {
-		if (is_corner_12<Greater>(p, w, *p+barrier)) 
-		    corners.push_back(p);
-	    } else {
-		if (is_corner_12<Less>(p, w, *p-barrier))
-		    corners.push_back(p);
-	    }
+    template <class C> inline bool is_corner_12(const byte* p, int w, int t) {
+	const int w3 = 3*w;
+	if (!C::eval(p[-3],t)) {
+	    if (!C::eval(p[-w3],t) || !C::eval(p[1-w3],t) || !C::eval(p[2-2*w],t) || !C::eval(p[3-w],t) || !C::eval(p[3],t) || !C::eval(p[3+w],t) || !C::eval(p[2+2*w],t) || !C::eval(p[1+w3],t) || !C::eval(p[w3],t))
+		return false;
+	    if (!C::eval(p[-1-w3],t))
+		return (C::eval(p[-1+w3],t) && C::eval(p[-2+2*w],t) && C::eval(p[-3+w],t));
+	    if (!C::eval(p[-2-2*w],t))
+		return (C::eval(p[-1+w3],t) && C::eval(p[-2+2*w],t));
+	    if (!C::eval(p[-3-w],t))
+		return (C::eval(p[-1+w3],t));
+	    return true;
 	}
-	BitCheck<I+1,N>::eval(three, p+1, w, barrier, corners);
+	if (!C::eval(p[-3-w],t)) {
+	    if (!C::eval(p[1-w3],t) || !C::eval(p[2-2*w],t) || !C::eval(p[3-w],t) || !C::eval(p[3],t) || !C::eval(p[3+w],t) || !C::eval(p[2+2*w],t) || !C::eval(p[1+w3],t) || !C::eval(p[w3],t) || !C::eval(p[-1+w3],t))
+		return false;
+	    if (!C::eval(p[-w3],t)  || !C::eval(p[-1-w3],t))
+		return (C::eval(p[-2+2*w],t) && C::eval(p[-3+w],t));
+	    if (!C::eval(p[-2-2*w],t))
+		return (C::eval(p[-2+2*w],t));
+	    return true;
+	}
+	if (!C::eval(p[-2-2*w],t)) {
+	    if (!C::eval(p[2-2*w],t) || !C::eval(p[3-w],t) || !C::eval(p[3],t) || !C::eval(p[3+w],t) || !C::eval(p[2+2*w],t) || !C::eval(p[1+w3],t) || !C::eval(p[w3],t) || !C::eval(p[-1+w3],t) || !C::eval(p[-2+2*w],t))
+		return false;
+	    if (C::eval(p[-3+w],t)) return true;
+	    return (C::eval(p[1-w3],t)  && C::eval(p[-w3],t) && C::eval(p[-1-w3],t));
+	}
+	if (!C::eval(p[-1-w3],t)) {
+	    return (C::eval(p[3-w],t) && C::eval(p[3],t) && C::eval(p[3+w],t) && C::eval(p[2+2*w],t) && C::eval(p[1+w3],t) && C::eval(p[w3],t) && C::eval(p[-1+w3],t) && C::eval(p[-2+2*w],t));
+	}
+	if (!C::eval(p[-w3],t)) {
+	    return (C::eval(p[3],t) && C::eval(p[3+w],t) && C::eval(p[2+2*w],t) && C::eval(p[1+w3],t) && C::eval(p[w3],t) && C::eval(p[-1+w3],t) && C::eval(p[-2+2*w],t) && C::eval(p[-3+w],t));
+	}
+	if (!C::eval(p[1-w3],t)) {
+	    return (C::eval(p[3+w],t) && C::eval(p[2+2*w],t) && C::eval(p[1+w3],t) && C::eval(p[w3],t) && C::eval(p[-1+w3],t) && C::eval(p[-2+2*w],t) && C::eval(p[-3+w],t));
+	}
+	if (!C::eval(p[2-2*w],t)) {
+	    return (C::eval(p[2+2*w],t) && C::eval(p[1+w3],t) && C::eval(p[w3],t) && C::eval(p[-1+w3],t) && C::eval(p[-2+2*w],t) && C::eval(p[-3+w],t));
+	}
+	if (!C::eval(p[3-w],t)) {
+	    return (C::eval(p[1+w3],t) && C::eval(p[w3],t) && C::eval(p[-1+w3],t) && C::eval(p[-2+2*w],t) && C::eval(p[-3+w],t));
+	}
+	if (!C::eval(p[3],t)) {
+	    return (C::eval(p[w3],t) && C::eval(p[-1+w3],t) && C::eval(p[-2+2*w],t) && C::eval(p[-3+w],t));
+	}
+	if (!C::eval(p[3+w],t)) {
+	    return (C::eval(p[-1+w3],t) && C::eval(p[-2+2*w],t) && C::eval(p[-3+w],t));
+	}
+	if (!C::eval(p[2+2*w],t)) {
+	    return (C::eval(p[-2+2*w],t) && C::eval(p[-3+w],t));
+	}
+	if (!C::eval(p[1+w3],t)) {
+	    return (C::eval(p[-3+w],t));
+	}
+	return true;
     }
-};
 
-template <int N> struct BitCheck<N,N> { 
-    template <class C> static inline void eval(unsigned int three, const byte* p, const int w, const int barrier, C& corners) {} 
-};
 
-template <int CHUNKS, class C> inline void process_16(unsigned int three, const byte* p, const int w, const int barrier, C& corners)
-{    
-    three |= (three >> 16);
-    const int BITS = 16/CHUNKS;
-    const int mask = ((1<<BITS)-1);
-    for (int i=0; i<CHUNKS; ++i) {
-	if (three & mask)
-	    BitCheck<0,BITS>::eval(three, p, w, barrier, corners);
-	p += BITS;
-	three >>= BITS;
+    template <int I, int N> struct BitCheck {
+	template <class C> static inline void eval(unsigned int three, const byte* p, const int w, const int barrier, C& corners) {
+	    const int BIT = 1<<I;
+	    if (three & BIT) {
+		if (three & (BIT << 16)) {
+		    if (is_corner_12<Greater>(p, w, *p+barrier)) 
+			corners.push_back(p);
+		} else {
+		    if (is_corner_12<Less>(p, w, *p-barrier))
+			corners.push_back(p);
+		}
+	    }
+	    BitCheck<I+1,N>::eval(three, p+1, w, barrier, corners);
+	}
+    };
+
+    template <int N> struct BitCheck<N,N> { 
+	template <class C> static inline void eval(unsigned int three, const byte* p, const int w, const int barrier, C& corners) {} 
+    };
+
+    template <int CHUNKS, class C> inline void process_16(unsigned int three, const byte* p, const int w, const int barrier, C& corners)
+    {    
+	three |= (three >> 16);
+	const int BITS = 16/CHUNKS;
+	const int mask = ((1<<BITS)-1);
+	for (int i=0; i<CHUNKS; ++i) {
+	    if (three & mask)
+		BitCheck<0,BITS>::eval(three, p, w, barrier, corners);
+	    p += BITS;
+	    three >>= BITS;
+	}
     }
-}
 
 #define CHECK_BARRIER(here, other, flags)				\
     {									\
@@ -118,64 +118,81 @@ template <int CHUNKS, class C> inline void process_16(unsigned int three, const 
 	asm( "pcmpgtb %%xmm7, %0  \n\t" : : "x"(diff));			\
 	flags = _mm_movemask_epi8(diff) | (_mm_movemask_epi8(diff2) << 16); \
     }
-
-void faster_corner_detect_12(const BasicImage<byte>& I, std::vector<ImageRef>& corners, int barrier)
-{
-    const int w = I.size().x;
-    const int stride = 3*w;
-    typedef std::list<const byte*> Passed;
-    Passed passed;
     
-    // Put the barrier in xmm7, which the compiler 
-    // does not seem to allocate
-    {   const __m128i barriers = _mm_set1_epi8((byte)barrier);
-	asm( "movdqa %0, %%xmm7  \n\t" : : "x"(barriers));  }
+    template <bool Aligned> inline __m128i load_si128(const void* addr) { return _mm_loadu_si128((const __m128i*)addr); }
+    template <> inline __m128i load_si128<true>(const void* addr) { return _mm_load_si128((const __m128i*)addr); }
+    
+    template <bool Aligned> void faster_corner_detect_12(const BasicImage<byte>& I, std::vector<ImageRef>& corners, int barrier)
+    {
+	const int w = I.size().x;
+	const int stride = 3*w;
+	typedef std::list<const byte*> Passed;
+	Passed passed;
+    
+	// Put the barrier in xmm7, which the compiler 
+	// does not seem to allocate
+	{   const __m128i barriers = _mm_set1_epi8((byte)barrier);
+	    asm( "movdqa %0, %%xmm7  \n\t" : : "x"(barriers));  }
 
-    for (int i=3; i<I.size().y-3; ++i) {
-	const byte* p = I[i];
-	for (int j=0; j<w/16; ++j, p+=16) {
-	    const __m128i here = _mm_load_si128((const __m128i*)(p));
-	    const __m128i above = _mm_load_si128((const __m128i*)(p-stride));
-	    const __m128i below = _mm_load_si128((const __m128i*)(p+stride));
-	    unsigned int up_flags, down_flags;
-	    CHECK_BARRIER(here, above, up_flags);
-	    CHECK_BARRIER(here, below, down_flags);
-	    const unsigned int either_ud = up_flags | down_flags;
-	    if (either_ud) {
-		unsigned int left_flags;
-		{
-		    const __m128i other = _mm_loadu_si128((const __m128i*)(p-3));
-		    CHECK_BARRIER(here, other, left_flags);
-		}
-		const unsigned int both_ud = up_flags & down_flags;
-		if (both_ud | (either_ud&left_flags)) {
-		    unsigned int right_flags;
+	for (int i=3; i<I.size().y-3; ++i) {
+	    const byte* p = I[i];
+	    for (int j=0; j<w/16; ++j, p+=16) {
+		const __m128i here = load_si128<Aligned>((const __m128i*)(p));
+		const __m128i above = load_si128<Aligned>((const __m128i*)(p-stride));
+		const __m128i below = load_si128<Aligned>((const __m128i*)(p+stride));
+		unsigned int up_flags, down_flags;
+		CHECK_BARRIER(here, above, up_flags);
+		CHECK_BARRIER(here, below, down_flags);
+		const unsigned int either_ud = up_flags | down_flags;
+		if (either_ud) {
+		    unsigned int left_flags;
 		    {
-			const __m128i other = _mm_loadu_si128((const __m128i*)(p+3));
-			CHECK_BARRIER(here, other, right_flags);
+			const __m128i other = _mm_loadu_si128((const __m128i*)(p-3));
+			CHECK_BARRIER(here, other, left_flags);
 		    }
-		    const unsigned int at_least_three = (either_ud & (left_flags & right_flags)) | (both_ud & (left_flags | right_flags));
-		    if (at_least_three) {
-			process_16<4>(at_least_three, p, w, barrier, passed);
+		    const unsigned int both_ud = up_flags & down_flags;
+		    if (both_ud | (either_ud&left_flags)) {
+			unsigned int right_flags;
+			{
+			    const __m128i other = _mm_loadu_si128((const __m128i*)(p+3));
+			    CHECK_BARRIER(here, other, right_flags);
+			}
+			const unsigned int at_least_three = (either_ud & (left_flags & right_flags)) | (both_ud & (left_flags | right_flags));
+			if (at_least_three) {
+			    process_16<4>(at_least_three, p, w, barrier, passed);
+			}
 		    }
 		}
 	    }
+	    passed.push_back(0);
 	}
-	passed.push_back(0);
-    }
-    corners.reserve(passed.size());
-    int row = 3;
-    const byte* row_start = I[3];
-    for (Passed::iterator it = passed.begin(); it != passed.end(); ++it) {
-	if (*it == 0) {
-	    row_start=I[++row];
-	    continue;
+	corners.reserve(passed.size());
+	int row = 3;
+	const byte* row_start = I[3];
+	for (Passed::iterator it = passed.begin(); it != passed.end(); ++it) {
+	    if (*it == 0) {
+		row_start=I[++row];
+		continue;
+	    }
+	    int x = *it - row_start;
+	    if (x > 2 && x < w-3)
+		corners.push_back(ImageRef(x, row));
 	}
-	int x = *it - row_start;
-	if (x > 2 && x < w-3)
-	    corners.push_back(ImageRef(x, row));
     }
-}
+
+    void faster_corner_detect_12(const BasicImage<byte>& I, std::vector<ImageRef>& corners, int barrier)
+    {
+	if (I.size().x < 22) {
+	    fast_corner_detect_12(I,corners,barrier);
+	    return;
+	} else if (I.size().x < 22 || I.size().y < 7)
+	    return;
+
+	if (is_aligned<16>(I[0]) && is_aligned<16>(I[1]))
+	    faster_corner_detect_12<true>(I, corners, barrier);
+	else
+	    faster_corner_detect_12<false>(I, corners, barrier);
+    }
 
 
 }
