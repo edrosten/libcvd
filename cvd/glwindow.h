8f920cb801823a6e71e88784a321eecc67f72a78
ethaneade <ethaneade>
2007-03-02 15:31:19 +0000

    Added GLWindow.  This is a richer implementation of VideoDisplay that hides the XEvent system from the user.
    The GLWindow class makes a window and an associated GL context.  When
    requested, events on the queue are either processed with a passed event
    handler (with callback virtual methods such as on_key_down and on_resize)
    or enumerated in a vector<GLWindow::Event> or GLWindow::EventSummary.
    
    Otherwise the functionality is the same as VideoDisplay.
    
    Feel free to add to the interface; I just got tired of linking against a
    separate library in order to have a reasonable display interface.

---
+++ cvd/glwindow.h
@@ -0,0 +1,128 @@
+#ifndef GLWINDOW_H
+#define GLWINDOW_H
+
+#include <string>
+#include <vector>
+#include <map>
+#include <cvd/image_ref.h>
+
+namespace CVD {
+    
+    namespace Exceptions
+    {
+	/// %Exceptions specific to CVD::GLWindow
+	/// @ingroup gException
+	namespace GLWindow
+	{
+	    /// Base class for all CVD::GLWindow exceptions
+	    /// @ingroup gException
+	    struct All: public CVD::Exceptions::All{
+	    };
+	    
+	    /// An exception occurred during initialisation
+	    /// @ingroup gException
+	    struct CreationError: public All
+	    {
+		CreationError(std::string w); ///< Construct from error string
+	    };
+	    
+	    /// An exception occurred during run-time
+	    /// @ingroup gException
+	    struct RuntimeError: public All
+	    {
+		RuntimeError(std::string w); ///< Construct from error string
+	    };
+	}
+    }
+    
+    /// An object that creates a window and a GL context attached to that window, and manages its events.
+    class GLWindow {
+    public:
+	/// Symbols for mouse buttons and modifiers
+	enum MouseButton { BUTTON_LEFT=1, BUTTON_MIDDLE=2, BUTTON_RIGHT=4, BUTTON_MOD_CTRL=8, BUTTON_MOD_SHIFT=0x10 };
+	/// Symbols for window events
+	enum EventType { EVENT_CLOSE };
+
+	/// Abstract base class for event handlers.  Subclass this and override to implement a handler.
+	class EventHandler {
+	public:
+	    virtual ~EventHandler() {}
+	    /// Called for key press events
+	    virtual void on_key_down(GLWindow& win, int key) {}
+	    /// Called for key release events
+	    virtual void on_key_up(GLWindow& win, int key) {}
+	    /// Called for mouse movement events
+	    virtual void on_mouse_move(GLWindow& win, ImageRef where, int state) {}
+	    /// Called for mouse button press events
+	    virtual void on_mouse_down(GLWindow& win, ImageRef where, int state, int button) {}
+	    /// Called for mouse button release events
+	    virtual void on_mouse_up(GLWindow& win, ImageRef where, int state, int button) {}
+	    /// Called for window resize events
+	    virtual void on_resize(GLWindow& win, ImageRef size) {}
+	    /// Called for general window events (such as EVENT_CLOSE)
+	    virtual void on_event(GLWindow& win, int event) {}
+	};
+
+	struct Event {
+	    enum Type { KEY_DOWN, KEY_UP, MOUSE_MOVE, MOUSE_DOWN, MOUSE_UP, RESIZE, EVENT };
+	    Type type;
+	    int which, state;
+	    ImageRef where, size;
+	};
+
+	/// A summary of multiple events
+	struct EventSummary {
+	    /// key->frequency mapping for key presses and releases
+	    std::map<int,int> key_down, key_up;
+	    /// button->frequency mapping for mouse presses and releases
+	    std::map<int,std::pair<ImageRef,int> > mouse_down, mouse_up;
+	    /// Generic window events -> frequency
+	    std::map<int,int> events;
+	    /// Reset the summary
+	    void clear() { *this = EventSummary(); }
+	    /// Has escape been pressed or the close button pressed?
+	    bool should_quit() const;
+	};
+
+	/// Construct a GLWindow of the given size and colour depth, with the given title.
+	/// A double-buffered GL context is associated with the window.
+	GLWindow(const ImageRef& size, int bpp, const std::string& title="GLWindow");
+	~GLWindow();
+	/// Get the size
+	ImageRef size() const;
+	/// Set the mouse cursor position
+	void set_cursor_position(const ImageRef& where);
+	/// Get the mouse cursor position
+	ImageRef cursor_position() const;
+	/// Show (or hide) the cursor
+	void show_cursor(bool show=true);
+	/// Hide the cursor
+	void hide_cursor() { show_cursor(false); }
+	/// Get the title
+	std::string title() const;
+	/// Set the title
+	void set_title(const std::string& title);
+	/// Swap the front and back buffers
+	void swap_buffers();
+	/// Handle events in the event queue by calling back to the specified handler.
+	void handle_events(EventHandler& handler);
+	/// Store all events in the event queue into Event objects.
+	void get_events(std::vector<Event>& events);
+	/// Make a summary of the events in the queue.
+	void get_events(EventSummary& summary);
+	/// @returns true if events are pending
+	bool has_events() const;
+	/// Make this GL context active
+	void activate();
+	/// Make this GL context active
+	void make_current() { activate(); }
+    private:
+	struct State;
+	State* state;
+    };
+
+    
+}
+
+
+#endif
