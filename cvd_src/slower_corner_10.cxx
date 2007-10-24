1632b9554b7e4a8f7b6b041d1de4940955fe6bec
edrosten <edrosten>
2007-10-24 18:04:53 +0000

    Changed definitions to match prototypes.
    The faster_* functions aren't SubImage based yet.

--- cvd_src/slower_corner_10.cxx
+++ cvd_src/slower_corner_10.cxx
@@ -3,7 +3,7 @@
 
 namespace CVD
 {
-	void fast_corner_detect_10(const SubImage<byte>& i, std::vector<ImageRef>& corners, int b)
+	void fast_corner_detect_10(const BasicImage<byte>& i, std::vector<ImageRef>& corners, int b)
 	{
 		fast_corner_detect_plain_10(i, corners, b);
 	}
--- cvd_src/slower_corner_12.cxx
+++ cvd_src/slower_corner_12.cxx
@@ -3,7 +3,7 @@
 
 namespace CVD
 {
-	void fast_corner_detect_12(const SubImage<byte>& i, std::vector<ImageRef>& corners, int b)
+	void fast_corner_detect_12(const BasicImage<byte>& i, std::vector<ImageRef>& corners, int b)
 	{
 		fast_corner_detect_plain_12(i, corners, b);
 	}
