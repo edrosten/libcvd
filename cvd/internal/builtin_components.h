0f6171f14cdb6311749e6f97d85d9d3b4c40d483
georgklein <georgklein>
2008-06-23 11:34:52 +0000

    Missing include for size_t

--- cvd/internal/builtin_components.h
+++ cvd/internal/builtin_components.h
@@ -11,7 +11,7 @@
 			 This library is distributed in the hope that it will be useful,
 			 but WITHOUT ANY WARRANTY; without even the implied warranty of
 			 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-			 Lesser General Public License for more details.
+    			 Lesser General Public License for more details.
 
 			 You should have received a copy of the GNU Lesser General Public
 			 License along with this library; if not, write to the Free Software
@@ -22,10 +22,14 @@
 #define CVD_BUILTIN_TRAITS_H_
 
 #include <limits>
+#include <cstddef>
 
 #if defined (CVD_HAVE_TOON)
 #include <TooN/TooN.h>
 #endif
+
+
+
 
 namespace CVD
 {
