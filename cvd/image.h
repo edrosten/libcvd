b1634ac9f1c37d28779fa81f014b99b6f99323af
gerhard <gerhard>
2009-04-15 09:46:38 +0000

    used wrong namespace

--- cvd/image.h
+++ cvd/image.h
@@ -389,7 +389,7 @@ template<class T> class SubImage
 		}
 
 		inline void copy_from( const SubImage<T> & other ){
-			CVD_IMAGE_ASSERT(other.size() == this->size(), ImageError::IncompatibleImageSizes);
+			CVD_IMAGE_ASSERT(other.size() == this->size(), Image::IncompatibleImageSizes);
 			std::copy(other.begin(), other.end(), this->begin());
 		}
 
