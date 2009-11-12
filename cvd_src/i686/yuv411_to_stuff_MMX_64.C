cf35c89c5138fa60447a519021023da6942b81a8
paulmcilroy <paulmcilroy>
2009-11-12 14:23:43 +0000

    Fixed a seg fault resulting from the use of the ebp register as a counter. Refactored yuv411_to_rgb to free up ebx and yuv411_to_y to free ecx for use instead of ebp.

--- cvd_src/i686/yuv411_to_stuff_MMX_64.C
+++ cvd_src/i686/yuv411_to_stuff_MMX_64.C
@@ -1,4 +1,4 @@
-/*                       
+/*
 	This file is part of the CVD Library.
 
 	Copyright (C) 2005 The Authors
@@ -24,30 +24,30 @@
 
 #include <cvd/colourspace.h>
 #include <cvd/config.h>
+#include <cvd/timer.h>
 
 namespace CVD
 {
 namespace ColourSpace
 {
 
-
 void yuv411_to_rgb_y(const unsigned char* in, int size, unsigned char* out, unsigned char *lum_out)
 {
 	const unsigned char* in_end = in + size * 6 / 4;
 
 	size /=8;
-	
+
 	__asm__ __volatile__(
 		//Put some zeros in mm7
 		"mov		$0, %%eax		\n\t"
 		"movd		%%eax, %%mm7	\n\t"
-	
+
 		//Load out and in
 		"mov		%0, %%rdi		\n\t"
 		"mov		%1, %%rsi		\n\t"
 		"mov		%[luma], %%r10	\n\t"
 		"push		%[end]			\n\t"
-		
+
 ".Lfoo:								\n\t"
 
 		"prefetchnta	128(%%rsi)	\n\t"
@@ -60,14 +60,14 @@ void yuv411_to_rgb_y(const unsigned char* in, int size, unsigned char* out, unsi
 		//Create the register mm0 = * bu guv rv
 		//Let ecx, edx contain (u-128), (v-128)
 
-		//Note the following 4 instructions are in a different order in the second 
+		//Note the following 4 instructions are in a different order in the second
 		//block, since it's faster to have them different (!)
 
-		"movzbl 3(%%rsi), %%edx		\n\t"	//dx = v   move-(with-zero-extend)-byte-(in to)-long 
+		"movzbl 3(%%rsi), %%edx		\n\t"	//dx = v   move-(with-zero-extend)-byte-(in to)-long
 		"sub	$128, %%edx			\n\t"
 		"movzbl	(%%rsi), %%ecx		\n\t"	//cx = u
-		"sub	$128, %%ecx			\n\t"	
-		
+		"sub	$128, %%ecx			\n\t"
+
 		"mov	%%edx, %%eax		\n\t"	//Calculate and insert rv = v * 147
 		"imul	$147, %%eax			\n\t"
 		"pinsrw	$0, %%eax, %%mm0	\n\t"
@@ -100,14 +100,14 @@ void yuv411_to_rgb_y(const unsigned char* in, int size, unsigned char* out, unsi
 		"pshufw $0x40, %%mm3, %%mm1	\n\t" //Create y2 y1 y1 y1 register mm1: 0x40 = 01 00 00 00
 		"pshufw $0xa5, %%mm3, %%mm2	\n\t" //Create y3 y3 y2 y2 register mm2: 0xa5 = 10 10 01 01
 		"pshufw $0xfe, %%mm3, %%mm3	\n\t" //Create y4 y4 y4 y3 register mm3: 0xfe = 11 11 11 10
-	
+
 
 		#ifdef CVD_HAVE_SSE2
 			"movnti	%%eax, (%%r10)		\n\t"
-		#else 
+		#else
 			"mov	%%eax, (%%r10)		\n\t"
 		#endif
-		
+
 		/////////////////////////////////////////////////////////////
 		//
 		// Perform fixed point conversion to RGB
@@ -117,7 +117,7 @@ void yuv411_to_rgb_y(const unsigned char* in, int size, unsigned char* out, unsi
 		//permute mm0 each time in to the right shape
 		"pshufw $0x24, %%mm0, %%mm0 \n\t"	//mm0 = r b g r    0x24 = 00 10 01 00
 		"paddw	%%mm0, %%mm1		\n\t"
-		
+
 		"pshufw $0x49, %%mm0, %%mm0	\n\t"	//mm0 = g r b g    0x49 = 01 00 10 01
 		"paddw	%%mm0, %%mm2		\n\t"
 
@@ -132,15 +132,15 @@ void yuv411_to_rgb_y(const unsigned char* in, int size, unsigned char* out, unsi
 
 		//Pack mm1,2 in to mm1  with unsigned saturation
 		"packuswb	%%mm2, %%mm1	\n\t"
-		
+
 		//At this point we only have 3 pixels, which won't leave the writing
 		//position in a 64 bit aligned state, so do another 3.
-	
-////////////////////////////////////////////////////////////////////////////////		
-////////////////////////////////////////////////////////////////////////////////		
-////////////////////////////////////////////////////////////////////////////////		
-////////////////////////////////////////////////////////////////////////////////		
-		//Perform the conversion on the second set of 3 pixels. 
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+		//Perform the conversion on the second set of 3 pixels.
 
 
 		////////////////////////////////////////////////////////////
@@ -154,8 +154,8 @@ void yuv411_to_rgb_y(const unsigned char* in, int size, unsigned char* out, unsi
 		"movzbl 9(%%rsi), %%edx		\n\t"	//dx = v
 		"movzbl	6(%%rsi), %%ecx		\n\t"	//cx = u   move-(with-zero-extend)-byte-(in to)-long
 		"sub	$128, %%edx			\n\t"
-		"sub	$128, %%ecx			\n\t"	
-		
+		"sub	$128, %%ecx			\n\t"
+
 		"mov	%%edx, %%eax		\n\t"	//Calculate and insert rv = v * 147
 		"imul	$147, %%eax			\n\t"
 		"pinsrw	$0, %%eax, %%mm0	\n\t"
@@ -174,20 +174,20 @@ void yuv411_to_rgb_y(const unsigned char* in, int size, unsigned char* out, unsi
 		//
 		// Load and convert luminance
 		//
-		
+
 		//Put y4y3y2y1 in to eax
 		"mov	8(%%rsi), %%eax		\n\t" //Load y4y3?? in to eax
 		"movw	7(%%rsi), %%ax		\n\t" //load y2y1 in to ax
-		
+
 		//Unpack and shift left by 7, so that mm5 contains the fixed point version of y4--y1
 		"movd	%%eax, %%mm6		\n\t"
 		"punpcklbw %%mm7, %%mm6		\n\t"
 		"psllw 	$7, %%mm6			\n\t"
-		
+
 
 		#ifdef CVD_HAVE_SSE2
 			"movnti %%eax, 4(%%r10)		\n\t"
-		#else 
+		#else
 			"mov %%eax, 4(%%r10)		\n\t"
 		#endif
 
@@ -205,7 +205,7 @@ void yuv411_to_rgb_y(const unsigned char* in, int size, unsigned char* out, unsi
 		//permute mm0 each time in to the right shape
 		"pshufw $0x24, %%mm0, %%mm0 \n\t"	//mm0 = r b g r    0x24 = 00 10 01 00
 		"paddw	%%mm0, %%mm4		\n\t"
-		
+
 		"pshufw $0x49, %%mm0, %%mm0	\n\t"	//mm0 = g r b g    0x49 = 01 00 10 01
 		"paddw	%%mm0, %%mm5		\n\t"
 
@@ -220,16 +220,16 @@ void yuv411_to_rgb_y(const unsigned char* in, int size, unsigned char* out, unsi
 		//Pack mm1,2 in to mm1 and mm3,4 in to mm3 with unsigned saturation
 		"packuswb	%%mm4, %%mm3	\n\t"
 		"packuswb	%%mm6, %%mm5	\n\t"
-		
+
 		//movntq uses write-combining. Fast.
 		"movntq		%%mm1, (%%rdi)	\n\t"
 		"movntq		%%mm3, 8(%%rdi)	\n\t"
 		"movntq		%%mm5, 16(%%rdi)\n\t"
 
 
-////////////////////////////////////////////////////////////////////////////////		
-////////////////////////////////////////////////////////////////////////////////		
-////////////////////////////////////////////////////////////////////////////////		
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
 
 		//Increment counters
 		"add		$12, %%rsi		\n\t"
@@ -244,6 +244,7 @@ void yuv411_to_rgb_y(const unsigned char* in, int size, unsigned char* out, unsi
 		: "eax", "ecx", "edx",  "rdi", "rsi", "mm0", "mm1", "mm2", "mm3", "mm4", "mm5", "mm6", "mm7", "r10"
 	);
 }
+
 void yuv411_to_rgb(const unsigned  char* in, int size, unsigned char* out)
 {
 	//Time: 5.0ms memcpy on number of luma pixels only takes 1.5 ms
@@ -252,16 +253,16 @@ void yuv411_to_rgb(const unsigned  char* in, int size, unsigned char* out)
 		//Put some zeros in mm7
 		"mov		$0, %%eax		\n\t"
 		"movd		%%eax, %%mm7	\n\t"
-	
+
 		//Load out and in
 		"mov		%0, %%rdi		\n\t"
 		"mov		%1, %%rsi		\n\t"
-		"mov		%2, %%ebp		\n\t"
+		"mov		%2, %%ebx		\n\t"
 
 ".Lyuv411dec:						\n\t"
-		
+
 		"prefetchnta	64(%%rsi)	\n\t"
-		/*Convert yuv411 to rgb8	
+		/*Convert yuv411 to rgb8
 		format is u1 y1 y2 v1 y3 y4
 		u, v are int8 - 128
 		y	   are uint8
@@ -278,7 +279,7 @@ void yuv411_to_rgb(const unsigned  char* in, int size, unsigned char* out)
 		The resultant 8 bit r,g,b values need to be saturated, since overflow occurs
 		and this leads to very ugly results.
 		*/
-		
+
 		////////////////////////////////////////////////////////////
 		//
 		//	Load and convert the chrominance values to mmxable things
@@ -287,21 +288,21 @@ void yuv411_to_rgb(const unsigned  char* in, int size, unsigned char* out)
 		//Create the register mm0 = * bu guv rv
 		//Let ecx, edx contain (u-128), (v-128)
 
-		//Note the following 4 instructions are in a different order in the second 
+		//Note the following 4 instructions are in a different order in the second
 		//block, since it's faster to have them different (!)
 
-		"movzbl 3(%%rsi), %%edx		\n\t"	//dx = v   move-(with-zero-extend)-byte-(in to)-long 
+		"movzbl 3(%%rsi), %%edx		\n\t"	//dx = v   move-(with-zero-extend)-byte-(in to)-long
 		"sub	$128, %%edx			\n\t"
 		"movzbl	(%%rsi), %%ecx		\n\t"	//cx = u
-		"sub	$128, %%ecx			\n\t"	
-		
+		"sub	$128, %%ecx			\n\t"
+
 		"mov	%%edx, %%eax		\n\t"	//Calculate and insert rv = v * 147
 		"imul	$147, %%eax			\n\t"
 		"pinsrw	$0, %%eax, %%mm0	\n\t"
 
-		"mov 	%%ecx, %%ebx		\n\t"	//Calculate and insert bu = v * 256
-		"imul	$256, %%ebx			\n\t"
-		"pinsrw $2, %%ebx, %%mm0	\n\t"
+		"mov 	%%ecx, %%eax		\n\t"	//Calculate and insert bu = v * 256
+		"imul	$256, %%eax			\n\t"
+		"pinsrw $2, %%eax, %%mm0	\n\t"
 
 		"imul	$-38, %%ecx			\n\t"	//Calculate and insert guv = -38u + -74v
 		"imul	$-74, %%edx			\n\t"
@@ -317,11 +318,11 @@ void yuv411_to_rgb(const unsigned  char* in, int size, unsigned char* out)
 		//
 		// Load and convert luminance
 		//
-		
+
 		//Put y4y3y2y1 in to eax
 		"mov	2(%%rsi), %%eax		\n\t" //Load y4y3?? in to eax
 		"movw	1(%%rsi), %%ax		\n\t" //load y2y1 in to ax
-		
+
 		//Unpack and shift left by 7, so that mm3 contains the fixed point version of y4--y1
 		"movd	%%eax, %%mm3		\n\t"
 		"punpcklbw %%mm7, %%mm3		\n\t"
@@ -341,7 +342,7 @@ void yuv411_to_rgb(const unsigned  char* in, int size, unsigned char* out)
 		//permute mm0 each time in to the right shape
 		"pshufw $0x24, %%mm0, %%mm0 \n\t"	//mm0 = r b g r    0x24 = 00 10 01 00
 		"paddw	%%mm0, %%mm1		\n\t"
-		
+
 		"pshufw $0x49, %%mm0, %%mm0	\n\t"	//mm0 = g r b g    0x49 = 01 00 10 01
 		"paddw	%%mm0, %%mm2		\n\t"
 
@@ -356,15 +357,15 @@ void yuv411_to_rgb(const unsigned  char* in, int size, unsigned char* out)
 
 		//Pack mm1,2 in to mm1  with unsigned saturation
 		"packuswb	%%mm2, %%mm1	\n\t"
-		
+
 		//At this point we only have 3 pixels, which won't leave the writing
 		//position in a 64 bit aligned state, so do another 3.
-	
-////////////////////////////////////////////////////////////////////////////////		
-////////////////////////////////////////////////////////////////////////////////		
-////////////////////////////////////////////////////////////////////////////////		
-////////////////////////////////////////////////////////////////////////////////		
-		//Perform the conversion on the second set of 3 pixels. 
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+		//Perform the conversion on the second set of 3 pixels.
 
 
 		////////////////////////////////////////////////////////////
@@ -378,15 +379,15 @@ void yuv411_to_rgb(const unsigned  char* in, int size, unsigned char* out)
 		"movzbl 9(%%rsi), %%edx		\n\t"	//dx = v
 		"movzbl	6(%%rsi), %%ecx		\n\t"	//cx = u   move-(with-zero-extend)-byte-(in to)-long
 		"sub	$128, %%edx			\n\t"
-		"sub	$128, %%ecx			\n\t"	
-		
+		"sub	$128, %%ecx			\n\t"
+
 		"mov	%%edx, %%eax		\n\t"	//Calculate and insert rv = v * 147
 		"imul	$147, %%eax			\n\t"
 		"pinsrw	$0, %%eax, %%mm0	\n\t"
 
-		"mov 	%%ecx, %%ebx		\n\t"	//Calculate and insert bu = v * 256
-		"imul	$256, %%ebx			\n\t"
-		"pinsrw $2, %%ebx, %%mm0	\n\t"
+		"mov 	%%ecx, %%eax		\n\t"	//Calculate and insert bu = v * 256
+		"imul	$256, %%eax			\n\t"
+		"pinsrw $2, %%eax, %%mm0	\n\t"
 
 		"imul	$-38, %%ecx			\n\t"	//Calculate and insert guv = -38u + -74v
 		"imul	$-74, %%edx			\n\t"
@@ -398,11 +399,11 @@ void yuv411_to_rgb(const unsigned  char* in, int size, unsigned char* out)
 		//
 		// Load and convert luminance
 		//
-		
+
 		//Put y4y3y2y1 in to eax
 		"mov	8(%%rsi), %%eax		\n\t" //Load y4y3?? in to eax
 		"movw	7(%%rsi), %%ax		\n\t" //load y2y1 in to ax
-		
+
 		//Unpack and shift left by 7, so that mm5 contains the fixed point version of y4--y1
 		"movd	%%eax, %%mm6		\n\t"
 		"punpcklbw %%mm7, %%mm6		\n\t"
@@ -422,7 +423,7 @@ void yuv411_to_rgb(const unsigned  char* in, int size, unsigned char* out)
 		//permute mm0 each time in to the right shape
 		"pshufw $0x24, %%mm0, %%mm0 \n\t"	//mm0 = r b g r    0x24 = 00 10 01 00
 		"paddw	%%mm0, %%mm4		\n\t"
-		
+
 		"pshufw $0x49, %%mm0, %%mm0	\n\t"	//mm0 = g r b g    0x49 = 01 00 10 01
 		"paddw	%%mm0, %%mm5		\n\t"
 
@@ -437,24 +438,24 @@ void yuv411_to_rgb(const unsigned  char* in, int size, unsigned char* out)
 		//Pack mm1,2 in to mm1 and mm3,4 in to mm3 with unsigned saturation
 		"packuswb	%%mm4, %%mm3	\n\t"
 		"packuswb	%%mm6, %%mm5	\n\t"
-		
+
 		//movntq uses write-combining. Fast.
 		"movntq		%%mm1, (%%rdi)	\n\t"
 		"movntq		%%mm3, 8(%%rdi)	\n\t"
 		"movntq		%%mm5, 16(%%rdi)\n\t"
 
 
-////////////////////////////////////////////////////////////////////////////////		
-////////////////////////////////////////////////////////////////////////////////		
-////////////////////////////////////////////////////////////////////////////////		
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
 
 		//Increment counters
 		"add		$24, %%rdi		\n\t"
 		"add		$12, %%rsi		\n\t"
-		"dec		%%ebp			\n\t"
+		"dec		%%ebx			\n\t"
 		"jnz		.Lyuv411dec		\n\t"
 		"emms						\n\t"	//End mmx
-	//   0          1        2   	
+	//   0          1        2
 	:
 	: "m" (out), "m" (in), "g" (size)
 		: "eax", "ecx", "edx", "rbx", "rdi", "rsi", "mm0", "mm1", "mm2", "mm3", "mm4", "mm5", "r10"
@@ -464,30 +465,30 @@ void yuv411_to_rgb(const unsigned  char* in, int size, unsigned char* out)
 void yuv411_to_y(const unsigned char* in, int size, unsigned char* out)
 {
 	//Time, 2.5ms. Comparison, memcpy on the same number of output bytes=1.5ms
-
 	size /=16;
 	__asm__ __volatile__(
 		"mov %0, %%rdi			\n\t"
 		"mov %1, %%rsi			\n\t"
-		"mov %2, %%ebp			\n\t"
+		"mov %2, %%ecx  		\n\t"
 ".Lyuvtolum:					\n\t"
 		"prefetchnta	64(%%rsi)	\n\t"
 		"mov  2(%%rsi), %%eax	\n\t"
 		"movw 1(%%rsi), %%ax	\n\t"
 		"mov  8(%%rsi), %%ebx	\n\t"
 		"movw 7(%%rsi), %%bx	\n\t"
-		"mov  14(%%rsi), %%ecx	\n\t"
-		"movw 13(%%rsi), %%cx	\n\t"
-		"mov  20(%%rsi), %%edx	\n\t"
-		"movw 19(%%rsi), %%dx	\n\t"
 
 		"movd	%%eax, %%mm0	\n\t"
 		"movd	%%ebx, %%mm1	\n\t"
 		"psllq  $32, %%mm1		\n\t"
 		"por	%%mm1, %%mm0	\n\t"  //mm0 = ebx,eax
 
-		"movd	%%ecx, %%mm1	\n\t"
-		"movd	%%edx, %%mm2	\n\t"
+		"mov  14(%%rsi), %%eax	\n\t"
+		"movw 13(%%rsi), %%ax	\n\t"
+		"mov  20(%%rsi), %%ebx	\n\t"
+		"movw 19(%%rsi), %%bx	\n\t"
+
+		"movd	%%eax, %%mm1	\n\t"
+		"movd	%%ebx, %%mm2	\n\t"
 		"psllq  $32, %%mm2		\n\t"
 		"por	%%mm2, %%mm1	\n\t"  //mm1 = ebx,eax
 
@@ -496,16 +497,14 @@ void yuv411_to_y(const unsigned char* in, int size, unsigned char* out)
 
 		"add  $24, %%rsi		\n\t"
 		"add  $16, %%rdi		\n\t"
-		"dec  %%ebp				\n\t"
+		"dec  %%ecx  			\n\t"
 		"jnz .Lyuvtolum			\n\t"
 		"emms					\n\t"
 	:
 		: "m" (out), "m" (in), "g" (size)
-		: "rax", "rcx", "rdi", "rsi", "mm0", "mm1", "mm2", "r10", "rbx"   );
+		: "rdi", "rsi", "mm0", "mm1", "mm2", "eax", "ebx", "ecx", "memory");
 }
 
 }
 }
-
-
 
