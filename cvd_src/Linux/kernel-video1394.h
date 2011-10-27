ba3a4849a758e8afb2957a69dfa6307f89256dbb
Dmitri Nikulin <dnikulin@gmail.com>
2011-10-27 14:17:41 +1100

    Extend kernel-video1394.h version checks to Linux 3.x

--- cvd_src/Linux/kernel-video1394.h
+++ cvd_src/Linux/kernel-video1394.h
@@ -53,24 +53,20 @@ struct cvd_video1394_wait
 
 //Define the ioctls
 
-#if CVD_KERNEL_MAJOR==2 
-	#if CVD_KERNEL_MINOR==6
-		#define LISTEN_CHANNEL		 	_IOWR('#', 0x10, struct cvd_video1394_mmap)
-		#define UNLISTEN_CHANNEL		_IOW ('#', 0x11, int)
-		#define LISTEN_QUEUE_BUFFER	 	_IOW ('#', 0x12, struct cvd_video1394_wait)
-		#define LISTEN_WAIT_BUFFER	 	_IOWR('#', 0x13, struct cvd_video1394_wait)
-		#define LISTEN_POLL_BUFFER	 	_IOWR('#', 0x18, struct cvd_video1394_wait)
-	#elif CVD_KERNEL_MINOR==4
-		#define LISTEN_CHANNEL  		0
-		#define UNLISTEN_CHANNEL  		1
-		#define LISTEN_QUEUE_BUFFER  	2
-		#define LISTEN_WAIT_BUFFER  	3
-		#define LISTEN_POLL_BUFFER  	8	 
-	#else
-		#define UNKNOWN_KERNEL
-	#endif
+#if (((CVD_KERNEL_MAJOR==2) && (CVD_KERNEL_MINOR==6)) || (CVD_KERNEL_MAJOR>=3))
+    #define LISTEN_CHANNEL          _IOWR('#', 0x10, struct cvd_video1394_mmap)
+    #define UNLISTEN_CHANNEL        _IOW ('#', 0x11, int)
+    #define LISTEN_QUEUE_BUFFER     _IOW ('#', 0x12, struct cvd_video1394_wait)
+    #define LISTEN_WAIT_BUFFER      _IOWR('#', 0x13, struct cvd_video1394_wait)
+    #define LISTEN_POLL_BUFFER      _IOWR('#', 0x18, struct cvd_video1394_wait)
+#elif ((CVD_KERNEL_MAJOR==2) && (CVD_KERNEL_MINOR==4))
+    #define LISTEN_CHANNEL          0
+    #define UNLISTEN_CHANNEL        1
+    #define LISTEN_QUEUE_BUFFER     2
+    #define LISTEN_WAIT_BUFFER      3
+    #define LISTEN_POLL_BUFFER      8
 #else 
-	#define UNKNOWN_KERNEL
+    #define UNKNOWN_KERNEL
 #endif
 
 #endif
