5353638cfe36164de046c04bd647f6a835799b53
Your Name <you@example.com>
2012-08-15 11:06:39 +0100

    Make null videofilebuffer work with new interface.
    Fix from Dinesh Gamage.

--- cvd_src/videosource_novideofilebuffer.cc
+++ cvd_src/videosource_novideofilebuffer.cc
@@ -1,12 +1,12 @@
 #include <cvd/videosource.h>
 namespace CVD{
 
-	template <> VideoBuffer<byte>* makeVideoFileBuffer(const std::string&, VideoBufferFlags::OnEndOfBuffer)
+	template <> VideoBuffer<byte>* makeVideoFileBuffer(const std::string&, VideoBufferFlags::OnEndOfBuffer, bool)
 	{
 		throw VideoSourceException("VideoFileBuffer is not compiled in to libcvd.");
 	}
 
-	template <> VideoBuffer<Rgb<byte> >* makeVideoFileBuffer(const std::string&, VideoBufferFlags::OnEndOfBuffer)
+	template <> VideoBuffer<Rgb<byte> >* makeVideoFileBuffer(const std::string&, VideoBufferFlags::OnEndOfBuffer, bool)
 	{
 		throw VideoSourceException("VideoFileBuffer is not compiled in to libcvd.");
 	}
