e0018c5ccc316dceb97a3d98560e95e0bafd04cd
Edward Rosten <ed@edwardrosten.com>
2015-07-09 13:49:15 +0100

    Remove two long deprecated files

--- cvd/cvd_timer.h
+++
@@ -1,8 +0,0 @@
-
-#ifdef __GNUC__
-#warning This file is deprecated. Use cvd/timer.h
-#else //elif defined(which ever compilers support this)
-#pragma warning "This file is deprecated. Use cvd/timer.h"
-#endif
-
-#include <cvd/timer.h>
