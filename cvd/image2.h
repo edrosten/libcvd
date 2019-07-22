503b4022a502bcebbe29cba477a9b20eb650b6fa
Edward Rosten <erosten@snapchat.com>
2019-07-22 16:15:56 +0100

    Fix for apple

---
+++ cvd/image2.h
@@ -0,0 +1,25 @@
+#ifndef CVD_IMAGE_H_9e7447dd4a4f1d760e7bbe8bfd961bf2
+#define CVD_IMAGE_H_9e7447dd4a4f1d760e7bbe8bfd961bf2
+
+namespace CVD
+{
+	class Default;
+	class Reference;
+
+	template<class Pixel, class Base=Default> class Image: public Image<Pixel, Reference>
+	{
+
+
+	}
+
+
+
+
+
+
+}
+
+
+
+#endif
+
